51cb4c0c1fb8da39bcfa90760af8579d7c6196a4
saikat1978 <saikatsakura@gmail.com>
2013-12-03 01:28:28 +0530

    twitter service implementation with search method

---
+++ src/java/techathon/service/AbstractTwitterService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techathon.service;
+
+import techathon.core.TwitterSession;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
+
+/**
+ *
+ * @author saikatsakura
+ */
+public class AbstractTwitterService {
+    
+    protected TwitterSession getAuthenticatedTwitterSession() throws TwitterException {
+        AccessToken accessToken = new AccessToken("27447997-DI7nEr57tcfJ9lSjgfPssPdOcRZuo6K3bDkL08xNl",
+                "hUQsknqfweVQumiNwjj6ZsfiDZbD28q2973sWvJdvalug");
+
+        Twitter session = TwitterFactory.getSingleton();
+        session.setOAuthAccessToken(null);
+        session.setOAuthConsumer("NOeCA0AeBPDMBrJxFj4BPQ", "AsltzXSiMi6GaEvMnMOe56bqrvMc09RVZrlQa5YG2jI");
+        User user = session.verifyCredentials();
+        TwitterSession twitterSession = new TwitterSession();
+        twitterSession.setAuthenticatedUser(user);
+        twitterSession.setTwitterSession(session);
+        return twitterSession;
+    }
+    
+}
---
+++ src/java/techathon/service/impl/TwitterServiceImpl.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techathon.service.impl;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.stereotype.Service;
+import techathon.core.TwitterSession;
+import techathon.service.AbstractTwitterService;
+import techathon.service.TwitterService;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+
+/**
+ *
+ * @author saikatsakura
+ */
+@Service
+public class TwitterServiceImpl extends AbstractTwitterService implements TwitterService {
+
+    public List<String> search(String text) {
+        List<String> tweets = new LinkedList<String>();
+        Query query = new Query(text);
+        query.setCount(100);
+        try {
+            TwitterSession session = getAuthenticatedTwitterSession();
+            QueryResult queryResult = session.getTwitterSession().search(query);
+            while (queryResult.hasNext()) {
+                for (Status s : queryResult.getTweets()) {
+                    tweets.add(s.getText());
+                }
+                queryResult = session.getTwitterSession().search(queryResult.nextQuery());
+            }
+        } catch (TwitterException ex) { 
+            Logger.getLogger(TwitterServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return tweets;
+    }
+    
+}
