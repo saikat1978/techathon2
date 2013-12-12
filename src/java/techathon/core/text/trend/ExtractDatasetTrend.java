4deecb3a434b05ae07a63c3e92e4581a0fc16bdd
saikat1978 <saikatsakura@gmail.com>
2013-12-12 16:44:31 +0530

    re-formatted code

--- src/java/techathon/core/text/trend/ExtractDatasetTrend.java
+++ src/java/techathon/core/text/trend/ExtractDatasetTrend.java
@@ -17,50 +17,46 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ExtractDatasetTrend
-{
+public class ExtractDatasetTrend {
     static final String DEF_INFILENAME = "ows.json";
     // Date pattern used to count the volume of tweets
     final SimpleDateFormat SDM = new SimpleDateFormat("dd MMM yyyy HH:mm");
 
-    public JSONArray GenerateDataTrend(String inFilename)
-    {
+    public JSONArray GenerateDataTrend(String inFilename) {
         BufferedReader br = null;
         JSONArray result = new JSONArray();
-        HashMap<String,Integer> datecount = new HashMap<String,Integer>();
-        try{
-            br= new BufferedReader(new InputStreamReader(new FileInputStream(inFilename),"UTF-8"));
+        HashMap<String, Integer> datecount = new HashMap<String, Integer>();
+        ArrayList<DateInfo> dinfos = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(inFilename), "UTF-8"));
             String temp = "";
-            while((temp = br.readLine())!=null)
-            {
+            while ((temp = br.readLine()) != null) {
                 try {
                     JSONObject jobj = new JSONObject(temp);
                     long timestamp = jobj.getLong("timestamp");
                     Date d = new Date(timestamp);
                     String strdate = SDM.format(d);
-                    if(datecount.containsKey(strdate))
-                    {
-                        datecount.put(strdate, datecount.get(strdate)+1);
-                    }
-                    else
-                    {
+                    if (datecount.containsKey(strdate)) {
+                        datecount.put(strdate, datecount.get(strdate) + 1);
+                    } else {
                         datecount.put(strdate, 1);
                     }
                 } catch (JSONException ex) {
                     Logger.getLogger(ExtractDatasetTrend.class.getName()).log(Level.SEVERE, null, ex);
-                }                
+                }
             }
             /** DateInfo consists of a date string and the corresponding count.
-              * It also implements a Comparator for sorting by date
-              */
-            ArrayList<DateInfo> dinfos = new ArrayList<DateInfo>();
+             * It also implements a Comparator for sorting by date
+             */
+            dinfos = new ArrayList<DateInfo>();
             Set<String> keys = datecount.keySet();
-            for(String key:keys)
-              DateInfo dinfo = new DateInfo();
+            for (String key : keys) {
+                DateInfo dinfo = new DateInfo();
                 try {
                     dinfo.d = SDM.parse(key);
                 } catch (ParseException ex) {
@@ -70,45 +66,43 @@ public class ExtractDatasetTrend
                 dinfo.count = datecount.get(key);
                 dinfos.add(dinfo);
             }
-            Collections.sort(dinfos);
-            // Format and return the date string and the corresponding count
-            for(DateInfo dinfo:dinfos)
-            {
-                try{
-                    JSONObject jobj = new JSONObject();
-                    jobj.put("date", SDM.format(dinfo.d));
-                    jobj.put("count", dinfo.count);
-                    result.put(jobj);
-                }catch(JSONException ex)
-                {
-                    ex.printStackTrace();
-                }
-            }
-        }catch(IOException ex)
-        {
+
+        } catch (IOException ex) {
             ex.printStackTrace();
-        }finally{
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
             try {
                 br.close();
             } catch (IOException ex) {
                 Logger.getLogger(ExtractDatasetTrend.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
+        Collections.sort(dinfos);
+        // Format and return the date string and the corresponding count
+        for (DateInfo dinfo : dinfos) {
+            try {
+                JSONObject jobj = new JSONObject();
+                jobj.put("date", SDM.format(dinfo.d));
+                jobj.put("count", dinfo.count);
+                result.put(jobj);
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+            }
+        }
         return result;
     }
 
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         ExtractDatasetTrend edt = new ExtractDatasetTrend();
-        
+
         String infilename = DEF_INFILENAME;
-        if(args!=null)
-        {
-            if(args.length>=1&&!args[0].isEmpty())
-            {
+        if (args != null) {
+            if (args.length >= 1 && !args[0].isEmpty()) {
                 File fl = new File(args[0]);
-                if(fl.exists())
-                {
+                if (fl.exists()) {
                     infilename = args[0];
                 }
             }
